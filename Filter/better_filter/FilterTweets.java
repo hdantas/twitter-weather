6cd645dce704fee28ff23652a9c43387b3c3e342
Henrique Dantas <hndantas@gmail.com>
2013-01-08 23:40:46 +0000

    Added flows results to data/ folder
    Former-commit-id: 7b8889999553a6cd735c63db10702d39ebc85d9b

---
+++ Filter/better_filter/FilterTweets.java
@@ -0,0 +1,54 @@
+import java.io.*;
+class FilterTweets 
+{
+   public static void main(String args[])
+	{
+
+	}
+
+// valid tweet:
+//count \t userID \t userName \t messageID \ Date \t latitude \t longitude \t source \t tweet \n
+
+
+	private void writeToFile(String filename, String linetowrite) {
+      try{
+		// Create file 
+		FileWriter fstream = new FileWriter(filename);
+        BufferedWriter out = new BufferedWriter(fstream);
+		out.write(linetowrite);
+		//Close the output stream
+		out.close();
+		} catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+
+	private void readFromFile(String filename) {
+		try{
+		// Open the file that is the first 
+		// command line parameter
+		FileInputStream fstream = new FileInputStream(filename);
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		//Read File Line By Line
+		while ((strLine = br.readLine()) != null) 	{
+			// Print the content on the console
+			System.out.println (strLine);
+		}
+		//Close the input stream
+		in.close();
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+//remove unpropper tabs and newlines
+//exclude tweets withou geoTag (with -1000,-1000)
+//exclude tweets outside netherlands (with coordinates outside 50 <lat< 55; 3 <long< 8)
+	private void cleanTweet(String tweet){
+
+
+	}
+}
+
--- Pig/flows.pig
+++ Pig/flows.pig
@@ -3,7 +3,7 @@ REGISTER bin/masti4.jar;
 DEFINE ConvertGPStoFlows masti4.ConvertGPStoFlows;
 
 -- Loads
-file = LOAD '../data/sample_grid.txt' USING PigStorage('\t') AS (count:long,userID:long,userName:chararray,messageID:long,date:chararray,gps_lat:double,gps_long:double,source:chararray,tweet:chararray,gpsblock:chararray);
+file = LOAD '../data/complete_grid.txt' USING PigStorage('\t') AS (count:long,userID:long,userName:chararray,messageID:long,date:chararray,gps_lat:double,gps_long:double,source:chararray,tweet:chararray,gpsblock:chararray);
 
 ordered_file = ORDER file BY count; -- order chronologically
 
@@ -18,8 +18,8 @@ grouped_flows = GROUP flattened_flows BY (sourceGPSBlock, destinationGPSBlock);
 count_grouped_flows = FOREACH grouped_flows GENERATE COUNT(flattened_flows) AS count, *;
 ordered_flows = ORDER count_grouped_flows BY count DESC;
 popular_flows = LIMIT ordered_flows 30;
-result = popular_flows;
 
-STORE flattened_flows INTO '$output_dir'; --write the result on Disk. $output_dir is a command line argument
+
+STORE popular_flows INTO '$output_dir'; --write the result on Disk. $output_dir is a command line argument
 -- STORE result INTO '$output_dir'; --write the result on Disk. $output_dir is a command line argument
 
